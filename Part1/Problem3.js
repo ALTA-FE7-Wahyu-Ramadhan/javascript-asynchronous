890afde3a00781f45af7c6e7a9f27202e30e489d
unknown <wahyuramadhan0549@gmail.com>
2022-07-07 00:08:41 +0700

    problem3 part1 solved

---
+++ Part1/Problem3.js
@@ -0,0 +1,29 @@
+//snakes and ladders game
+const shallPassed = 30;
+let steps = 0;
+function generateRandomSteps() {
+  //don't touch it
+  return Math.ceil(Math.random() * 10);
+}
+function generateRandomSecond() {
+    //don't touch it
+  //in milliseconds
+  return Math.ceil(Math.random() * 10) * 1000;
+}
+
+function snakesAndLaddersGame(stepNow, seconds, callback) {
+    let step = stepNow;
+    let second = seconds;
+    let interval = setInterval(() => {
+        if (step >= shallPassed) {
+            clearInterval(interval);
+            callback(true);
+        } else {
+      step += generateRandomSteps();
+      second += generateRandomSecond();
+      setTimeout(() => {
+        snakesAndLaddersGame(step, second, callback);
+    }, second);
+}
+}, second);
+}
